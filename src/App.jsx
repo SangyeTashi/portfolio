f196821826738f509a04baa313cd083ef1db9203
sangyetashi <sangyetashi2@gmail.com>
2023-04-21 20:33:21 +0530

    add base style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Content from './Content';
 
 function App() {
     return (
-        <div className="bg-primary text-white h-screen relative overflow-y-scroll">
+        <div className="h-screen ">
             <div className="max-w-5xl mx-auto relative">
                 <Header />
 
